ae69187c55c2c466fb6f6a629cd48c722df43645
niranjangutha <niranjannaidu1@gmail.com>
2019-06-15 12:51:43 +0530

    add stringcapital data

--- src/StringConcepts/StringCapital.java
+++ src/StringConcepts/StringCapital.java
@@ -1,5 +1,29 @@
 package StringConcepts;
 
 public class StringCapital {
+	
+	/*
+	 * change words starts with small to capital
+	 */
+
+		public static void main(String[] args) {
+
+			String s = "my name is rohit";
+			String rev = "";
+			
+			String s1 [] = s.split(" ");
+			for(int i =0; i<s1.length; i++){
+			String f = s1[i].substring(0, 1).toUpperCase();
+			String l = s1[i].substring(1, s1[i].length());
+			
+			rev = rev +" "+  (f+l);
+			
+			
+			}
+			System.out.println(rev.trim());
+			
+
+		}
+
 
 }
