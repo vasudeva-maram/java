67c2fd0d05395193e492c233c2238cc654b887a1
vasu <vasudeva101994@gmail.com>
2019-07-05 22:51:18 +0530

    testing

--- src/StringConcepts/StringCapital.java
+++ src/StringConcepts/StringCapital.java
@@ -15,7 +15,7 @@ public class StringCapital {
 			for(int i =0; i<s1.length; i++){
 			String f = s1[i].substring(0, 1).toUpperCase();
 			String l = s1[i].substring(1, s1[i].length());
-			
+			System.out.print("This is Vasu");
 			rev = rev +" "+  (f+l);
 			
 			
